4218d47cd2b7880fe8c1cb9007fb865abcc08d1e
David_Ospinas <davidalexanderospinaguerrero1@gmail.com>
2025-11-21 17:00:23 -0500

    server.js

--- server.js
+++ server.js
@@ -66,8 +66,7 @@ app.delete('/tasks/:id', (req, res) => {
   stmt.finalize();
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor corriendo en puerto ${port}`);
 });
 
